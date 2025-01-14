52ecf4b7c6857fb4f9db1c4be6737ec42e6795e2
avinash23em <ap23vi@gmail.com>
2025-01-14 12:22:21 +0530

    intial

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-"use strict";
-let x = 1;
+var x = 1;
 console.log(x);
+var authorone = { name: 'mario', avatar: '.mario.png' };
+var newpost = {
+    name: 'idk', author: authorone, title: 'something', tags: ['tech', 'fast']
+};
+function create(post) {
+    console.log("this post  ".concat(post.title, " is created by ").concat(post.author.name));
+}
+create(newpost);
--- index.ts
+++ index.ts
@@ -1,3 +1,30 @@
 let x:number =1;
 
 console.log(x);
+
+
+interface Author {
+    name:string,
+   avatar:string
+}
+
+const authorone:Author={name:'mario',avatar:'.mario.png'}
+
+interface Post{
+    name:string,
+    author:Author,
+    title:string,
+    tags:string[]
+}
+
+
+const newpost:Post={
+    name:'idk',author:authorone,title:'something',tags:['tech','fast']
+}
+
+
+function create(post:Post): void {
+    console.log(`this post  ${post.title} is created by ${post.author.name}`)
+}
+
+create(newpost);
