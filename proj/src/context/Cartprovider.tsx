d21397f8f836e3611f748c7b910571d981245287
avinash23em <ap23vi@gmail.com>
2025-02-15 21:29:56 +0530

    intial

--- proj/src/context/Cartprovider.tsx
+++ proj/src/context/Cartprovider.tsx
@@ -1,5 +1,5 @@
 export type cartitemtype={
-    snu:string,
+    sku:string,
     name:string,
     price:number,
     qty:number,
@@ -21,4 +21,60 @@ export type ReducerActionType = typeof REDUCER_ACTION_TYPE
 export type ReducerAction = {
     type: string,
     payload?: cartitemtype,
+}
+
+
+const reducer = (state: cartstatetype, action: ReducerAction): cartstatetype => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPE.ADD: {
+            if (!action.payload) {
+                throw new Error('action.payload missing in ADD action')
+            }
+
+            const { sku, name, price } = action.payload
+
+            const filteredCart: cartitemtype[] = state.cart.filter(item => item.sku !== sku)
+
+            const itemExists: cartitemtype | undefined = state.cart.find(item => item.sku === sku)
+
+            const qty: number = itemExists ? itemExists.qty + 1 : 1
+
+            return { ...state, cart: [...filteredCart, { sku, name, price, qty }] }
+        }
+        case REDUCER_ACTION_TYPE.REMOVE: {
+            if (!action.payload) {
+                throw new Error('action.payload missing in REMOVE action')
+            }
+
+            const { sku } = action.payload
+
+            const filteredCart: cartitemtype[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...filteredCart] }
+        }
+        case REDUCER_ACTION_TYPE.QUANTITY: {
+            if (!action.payload) {
+                throw new Error('action.payload missing in QUANTITY action')
+            }
+
+            const { sku, qty } = action.payload
+
+            const itemExists: cartitemtype | undefined = state.cart.find(item => item.sku === sku)
+
+            if (!itemExists) {
+                throw new Error('Item must exist in order to update quantity')
+            }
+
+            const updatedItem: cartitemtype = { ...itemExists, qty }
+
+            const filteredCart: cartitemtype[] = state.cart.filter(item => item.sku !== sku)
+
+            return { ...state, cart: [...filteredCart, updatedItem] }
+        }
+        case REDUCER_ACTION_TYPE.SUBMIT: {
+            return { ...state, cart: [] }
+        }
+        default:
+            throw new Error('Unidentified reducer action type')
+    }
 }
